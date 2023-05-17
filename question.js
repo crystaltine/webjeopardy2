3b830178ae7c8fb17b275be03f324a2969d7241f
cantthinkofausername16 <67755226+cantthinkofausername16@users.noreply.github.com>
2023-05-17 01:49:05 -0700

    Updates

--- question.js
+++
@@ -1,8 +0,0 @@
-const params = (new URL(document.location)).searchParams;
-const cid = params.get("cid");
-const iid = params.get("iid");
-const gameName = params.get("gamename");
-
-console.log(`gname=${gameName} cid: ${cid}, iid: ${iid}`);
-
-document.getElementById("root").textContent = `q: ${JSON.parse(localStorage.getItem(gameName)).content[cid].colItems[iid].question}`;
